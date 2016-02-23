0cef7deb7e1f5a562640dee4fcf015a5860aa3a4
stephanmg <stephan@syntaktischer-zucker.de>
2016-02-23 18:45:53 +0100

    Some improvements.

--- trigger.sh
+++ trigger.sh
@@ -3,7 +3,8 @@
 ## If the branch name of the upstream and downstream project are equal and
 ## we have a valid travis configuration in the downstream project then
 ## try to trigger a travis build with the given github/travis user and
-## the given github/travis access token
+## the given github/travis access token and log a certain message in the
+## travis downstream project build for tracing back the dependent builds
 
 # arguments
 USER=$1
@@ -13,14 +14,14 @@ TRAVIS_ACCESS_TOKEN=$4
 MESSAGE=$5
 
 # check for correct input
-if [[ $# < 4 ]] && usage
+if [[ $# < 4 ]] && usage()
 
 # trigger build if above conditions hold
 if [[ ($TRAVIS_BRANCH == $3) &&
    ($TRAVIS_PULL_REQUEST == false) &&
    ( (! $TRAVIS_JOB_NUMBER == *.*) || ($TRAVIS_JOB_NUMBER == *.1) ) ]] ; then
    chmod +x trigger-travis.sh
-   ./trigger-travis.sh $1 $2 $4 $3
+   ./trigger-travis.sh $1 $2 $4 $3 $5
 fi
 
 # usage function
